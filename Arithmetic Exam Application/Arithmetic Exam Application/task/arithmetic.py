5bfd85cd1b5bbd53fffe1115fbae11dfd7224a72
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-11-14 19:33:55 +0400

    Arithmetic Exam Application - Stage 2/4

--- Arithmetic Exam Application/Arithmetic Exam Application/task/arithmetic.py
+++ Arithmetic Exam Application/Arithmetic Exam Application/task/arithmetic.py
@@ -1,13 +1,39 @@
-a, operation, b = input().split(" ")
-op1 = int(a)
-op2 = int(b)
+import random
+
+# a, operation, b = input().split(" ")
+# op1 = int(a)
+# op2 = int(b)
+#
+# result = None
+# if operation == "+":
+#     result = op1 + op2
+# elif operation == "*":
+#     result = op1 * op2
+# elif operation == "-":
+#     result = op1 - op2
+#
+# print(result)
+
+op1 = random.randint(2, 9)
+op2 = random.randint(2, 9)
+operation = random.randint(1, 3)
 
 result = None
-if operation == "+":
+op = ""
+if operation == 1:
+    op = "+"
     result = op1 + op2
-elif operation == "*":
-    result = op1 * op2
-elif operation == "-":
+elif operation == 2:
+    op = "-"
     result = op1 - op2
+elif operation == 3:
+    op = "*"
+    result = op1 * op2
 
-print(result)
+print(op1, op, op2)
+result2 = int(input())
+
+if result == result2:
+    print("Right!")
+else:
+    print("Wrong!")
---
+++ Arithmetic Exam Application/Topics/Slicing/You've got mail/main.py
@@ -0,0 +1,4 @@
+# write your code here
+# remember: the variable email is already defined
+name, domain = email.split("@")
+print(name)
