bfcde20c77693b9b1b72223f1400756f3d9d28fc
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-11-18 10:29:13 +0400

    Bill Splitter - Stage 3/4

--- Bill Splitter/Bill Splitter/task/billsplitter.py
+++ Bill Splitter/Bill Splitter/task/billsplitter.py
@@ -1,3 +1,5 @@
+import random
+
 friends = {}
 
 
@@ -7,7 +9,6 @@ def add_friends():
     print("Enter the number of friends joining (including you):")
     n = int(input())
     if n <= 0:
-        print("No one is joining for the party")
         return 0
 
     print("Enter the name of every friend (including you), each on a new line:")
@@ -15,19 +16,46 @@ def add_friends():
         name = input()
         friends[name] = 0
 
+    return n
 
-def update_bill(n):
+
+def update_bill(n, lucky=False):
     global friends
 
     for friend in friends:
-        friends[friend] = n
+        if lucky and friend == lucky:
+            friends[friend] = 0
+        else:
+            friends[friend] = n
 
 
-if add_friends() != 0:
+def main():
+    global friends
+
+    n = add_friends()
+    if n == 0:
+        print("No one is joining for the party")
+        return
+
     print("Enter the total bill value:")
     bill = int(input())
-    bill_for_each = round(bill / len(friends), 2)
-    update_bill(bill_for_each)
 
-    print()
-    print(friends)
+    print('Do you want to use the "Who is lucky?" feature? Write Yes/No:')
+    answer = input()
+
+    bill_for_each = 0
+    if answer == "Yes" and n != 1:
+        bill_for_each = round(bill / (n - 1), 2)
+        lucky = list(friends.keys())[random.randint(0, n - 1)]
+        print(f"{lucky} is the lucky one!")
+        update_bill(bill_for_each, lucky)
+    else:
+        bill_for_each = round(bill / n, 2)
+        print("No one is going to be lucky")
+        update_bill(bill_for_each)
+
+    # print()
+    # print(friends)
+
+
+main()
--- Bill Splitter/Bill Splitter/task/test/tests.py
+++ Bill Splitter/Bill Splitter/task/test/tests.py
@@ -2,15 +2,18 @@ from hstest.stage_test import *
 from hstest.test_case import TestCase
 import ast, math
 
+END_RESULT = "No one is going to be lucky"
 INVALID_RESULT = "No one is joining for the party"
 
 
 class BillSplitterTest(StageTest):
     def generate(self) -> List[TestCase]:
-        return [TestCase(stdin=["5", "Marc", "Jem", "Monica", "Anna", "Sam", "100"], attach=["5", "100"]),
-                TestCase(stdin=["7", "Marc", "Jem", "Monica", "Anna", "Jason", "Ben", "Ned", "41"], attach=["7", "41"]),
-                TestCase(stdin=["3", "Jake", "Sam", "Irina", "109"], attach=["3", "109"]),
-                TestCase(stdin=["2", "Jake", "Sam", "109"], attach=["2", "109"]),
+        return [TestCase(stdin=["5", "Marc", "Jem", "Monica", "Anna", "Jason", "100", "Yes"],
+                         attach=["5", "100", "Yes", ["Marc", "Jem", "Monica", "Anna", "Jason"]]),
+                TestCase(stdin=["3", "Jake", "Sam", "Irina", "109", "No"],
+                         attach=["3", "109", "No", ["Jake", "Sam", "Irina"]]),
+                TestCase(stdin=["2", "Jake", "Sam", "109", "No"],
+                         attach=["2", "109", "No", ["Jake", "Sam"]]),
                 TestCase(stdin=["0", "100"], attach=["0", "100"]),
                 TestCase(stdin=["-1", "-5"], attach=["-1", "-5"])
                 ]
@@ -23,33 +26,18 @@ class BillSplitterTest(StageTest):
             elif strings[1] != INVALID_RESULT:
                 return CheckResult.wrong("Expected output is - No one is joining for the party")
             return CheckResult.correct()
-        elif int(attach[0]) > 0 and (len(strings) != 4):
-            return CheckResult.wrong("Your code is not printing expected number of lines of output, check examples")
-        try:
-            output = ast.literal_eval(strings[3])
-        except ValueError:
-            return CheckResult.wrong("Please check your output, it should be a dictionary")
-        except IndentationError:
-            return CheckResult.wrong('There should not be any leading whitespace before your last output')
-        except Exception:
-            return CheckResult.wrong('Something wrong with your output. '
-                                     'Make sure you print the dictionary like in examples!\n'
-                                     f'Found dict: \n{strings[3]}')
-        if (not isinstance(output, dict)):
-            return CheckResult.wrong("Please use Dictionary data structure to store user input")
-        elif (len(output) != int(attach[0])):
-            return CheckResult.wrong(
-                "Please check if you have added all your friends to dictionary after taking an user input")
-        try:
-            bill_list = list(output.values())
-            bill = sum(bill_list)
-        except TypeError:
-            return CheckResult.wrong("Dictionary Values should be of integer type")
-        if (len(output) != 0 and (math.ceil(bill) != float(attach[1]) and math.floor(bill) != float(attach[1]))):
-            return CheckResult.wrong("Please update dictionary with correct split values")
-        elif (len(output) != 0 and bill_list[0] != round(int(attach[1]) / int(attach[0]), 2)):
-            return CheckResult.wrong("Please round off split values to two decimal places")
 
+        elif int(attach[0]) > 0 and len(strings) != 5:
+            return CheckResult.wrong("Your code is not printing expected number of lines of output, check examples")
+        elif (attach[2] == "Yes"):
+            lucky_string = strings[4].split(" ")
+            name = lucky_string[0]
+            if (name not in attach[3]):
+                return CheckResult.wrong(
+                    "Expected output is a random name from dictionary keys, but we got something else")
+        elif (attach[2] == "No"):
+            if strings[4] != END_RESULT:
+                return CheckResult.wrong("Output should be - No one is going to be lucky")
         return CheckResult.correct()
 
 
