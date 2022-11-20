00808bd7241bc04b51ffa473d0d00825a5edca80
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-11-20 16:29:05 +0400

    Password Hacker - Stage 2/5

--- Password Hacker/Password Hacker/task/hacking/hack.py
+++ Password Hacker/Password Hacker/task/hacking/hack.py
@@ -1,17 +1,39 @@
 import sys
 import socket
+from itertools import product
+import string
+
+
+CHARS = string.ascii_lowercase + string.digits
 
 args = sys.argv
 hostname = args[1]
 port = int(args[2])
-password = args[3]
 
 client_socket = socket.socket()
 address = (hostname, port)
 client_socket.connect(address)
-data = password.encode()
-client_socket.send(data)
-response = client_socket.recv(1024)
-response = response.decode()
+
+
+def find_pass():
+    for i in range(1, 1000):
+        for v in product(CHARS, repeat=i):
+            password = "".join(v)
+            data = password.encode()
+            client_socket.send(data)
+            response = client_socket.recv(1024)
+            response = response.decode()
+            if response == "Wrong password!":
+                continue
+            elif response == "Connection success!":
+                return password
+            elif response == "Too many attempts":
+                return
+
+
+result = find_pass()
 client_socket.close()
-print(response)
+
+if result:
+    print(result)
+
--- Password Hacker/Password Hacker/task/test/tests.py
+++ Password Hacker/Password Hacker/task/test/tests.py
@@ -1,9 +1,9 @@
 from hstest.stage_test import StageTest
 from hstest.test_case import TestCase
 from hstest.check_result import CheckResult
-from hstest.exceptions import WrongAnswerException
 from threading import Thread
 from time import sleep
+from itertools import product
 import socket
 import random
 
@@ -11,6 +11,14 @@ CheckResult.correct = lambda: CheckResult(True, '')
 CheckResult.wrong = lambda feedback: CheckResult(False, feedback)
 
 abc = 'abcdefghijklmnopqrstuvwxyz1234567890'
+
+
+def generate_password():
+    index = 1
+    while True:
+        abc = 'abcdefghijklmnopqrstuvwxyz1234567890'
+        yield from product(abc, repeat=index)
+        index += 1
 
 
 def random_password():
@@ -27,6 +35,7 @@ class Hacking(StageTest):
         self.serv = None
         self.connected = False
         self.message = []
+        self.password = None
 
     def start_server(self):
         self.serv = Thread(target=lambda: self.server())
@@ -43,7 +52,7 @@ class Hacking(StageTest):
         self.serv.join()
 
     def server(self):
-        ''' creating a server and answering clients '''
+        '''function - creating a server and answering clients'''
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind(('localhost', 9090))
@@ -55,40 +64,50 @@ class Hacking(StageTest):
             conn.settimeout(15)
             while True:
                 data = conn.recv(1024)
+                self.message.append(data.decode('utf8'))
+                if len(self.message) > 1_000_000:
+                    conn.send('Too many attempts to connect!'.encode('utf8'))
+                    break
                 if not data:
                     break
-                self.message.append(data.decode('utf8'))
-                conn.send('Wrong password!'.encode('utf8'))
+                if data.decode('utf8') == self.password:
+                    conn.send('Connection success!'.encode('utf8'))
+                else:
+                    conn.send('Wrong password!'.encode('utf8'))
             conn.close()
         except:
             pass
 
     def generate(self):
+        self.message = []
+        self.password = random_password()
         self.start_server()
-        test_word = random_password()
         return [
-            TestCase(
-                args=['localhost', '9090', test_word], attach=[test_word])
+            TestCase(args=['localhost', '9090'],
+                     attach=[self.password], time_limit=25000)
         ]
 
     def check(self, reply, attach):
-        self.stop_server()
 
         if not self.connected:
             return CheckResult.wrong("You didn't connect to the server")
-        if len(self.message) == 0:
-            return CheckResult.wrong('You sent nothing to the server')
-        if len(reply) == 0:
-            return CheckResult.wrong(
-                'You did not print anything')
-        if reply.split('\n')[0] != 'Wrong password!':
-            return CheckResult.wrong(
-                'The line you printed is not the one sent by server')
 
-        if self.message != attach:
-            return CheckResult.wrong(
-                'You sent the wrong information to the server')
-        return CheckResult.correct()
+        real_password = attach[0]
+        if reply.split('\n')[0] != real_password:
+            return CheckResult.wrong(f'The password you printed is not correct. The password is "{real_password}"')
+        success = True
+        for i in generate_password():
+            if len(i) == len(real_password):
+                break
+            try:
+                self.message.remove(''.join(i))
+            except ValueError:
+                success = False
+                break
+        if success:
+            return CheckResult.correct()
+        return CheckResult.wrong(
+            'Your generator algorithm does not include all the variants')
 
 
 if __name__ == '__main__':
